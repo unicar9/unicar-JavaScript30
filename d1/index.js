07c8aae1fea211bd9739ce594f042a2d86e7f0f0
Unicar <weijia@baseup.com.au>
2018-02-05 21:26:42 +1100

    Work on colors

--- d1/index.js
+++ d1/index.js
@@ -1,9 +1,23 @@
+const keyNamePairs = {
+    65: 'CLAP',
+    83: 'HIHAT',
+    68: 'KICK',
+    70: 'OPENHAT',
+    71: 'BOOM',
+    72: 'RIDE',
+    74: 'SNARE',
+    75: 'TOM',
+    76: 'TINK'
+}
+
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
-  console.log(key);
 
   if (!audio) return;
+
+  const soundName = document.querySelector('.sound-name')
+  soundName.innerHTML = keyNamePairs[e.keyCode]
 
   audio.currentTime = 0;
   audio.play();
