f5234abc157f472730924ecedad9bc8b919daaf2
Unicar <weijia@baseup.com.au>
2018-02-07 15:40:06 +1100

    Added responsiveness to the clock

--- d2/index.js
+++ d2/index.js
@@ -7,15 +7,15 @@ window.addEventListener("DOMContentLoaded", () => {
         const now = new Date();
 
         const seconds = now.getSeconds();
-        const secondsDegrees = ((seconds / 60) * 360) + 90;
+        const secondsDegrees = ((seconds / 60) * 360) + 180;
         secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
         const mins = now.getMinutes();
-        const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+        const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 180;
         minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
         const hour = now.getHours();
-        const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+        const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 180;
         hourHand.style.transform = `rotate(${hourDegrees}deg)`;
     }
 
