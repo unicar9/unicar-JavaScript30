236214cb071b1e779a1f6f76fc8881dc510f68bf
Unicar <unicar9@gmail.com>
2018-02-16 17:54:01 +1100

    Work on d11

--- d11/index.js
+++ d11/index.js
@@ -42,8 +42,10 @@ video.addEventListener('timeupdate', handleProgress)
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip))
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+ranges.forEach(range => {
+    range.addEventListener('change', handleRangeUpdate)
+    range.addEventListener('mousemove', handleRangeUpdate)
+})
 
 let mousedown = false
 progress.addEventListener('click', scrub)
