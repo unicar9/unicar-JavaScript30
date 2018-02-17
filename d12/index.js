69d105d010d40fd149602d14a9565e419bd7b82d
Unicar <unicar9@gmail.com>
2018-02-17 21:27:07 +1100

    Added d12

--- d12/index.js
+++ d12/index.js
@@ -4,22 +4,31 @@ function randy(min, max) {
 
 function addDoggo() {
   const $img = document.createElement('img')
-  $img.src = images[randy(0, 25)]
+  $img.src = images[randy(0, 25)].images.fixed_width.url
   $img.style.position = 'absolute'
-  $img.style.top = `${randy(0, 100)}%`
-  $img.style.left = `${randy(0, 100)}%`
+  $img.style.top = `calc(${randy(0, 100)}% + 100px)`
+  $img.style.left = `calc(${randy(0, 100)}% + 100px)`
+  $img.style.transform = `rotate(${randy(0, 360)}deg)`
 
   $container.appendChild($img)
 }
 
+function colorChange() {
+  $container.style.background = `hsl(${randy(0, 361)}, 80%, 80%)`
+}
 
-const endpoint = "http://api.giphy.com/v1/gifs/search?q=doggo&api_key=EXnlBIXZEt8KY8fIVINc1mxeZBBxUjof"
+
+const endpoint = "http://api.giphy.com/v1/gifs/search?q=puppy&api_key=EXnlBIXZEt8KY8fIVINc1mxeZBBxUjof"
 const $container = document.querySelector('.container')
 
 const images = []
 fetch(endpoint)
     .then(blob => blob.json())
-    .then(data => images.push(...data.images.downsized_large.url))
+    .then(data => {
+      images.push(...data.data)
+      console.log(images)
+      console.log("Fetch down!!!!")
+    })
 
 const pressed = []
 const secretCode = 'unicar'
@@ -31,5 +40,6 @@ window.addEventListener('keyup', (e) => {
   if (pressed.join('').includes(secretCode)) {
     console.log("芝麻开门")
     addDoggo()
+    colorChange()
   }
 })
