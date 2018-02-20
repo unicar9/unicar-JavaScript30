03960d6c9fda32d4ee4a8a9f70a498e4ca502da9
Unicar <weijia@baseup.com.au>
2018-02-20 17:37:33 +1100

    Work on d14 func

--- d14/index.js
+++ d14/index.js
@@ -1,22 +1,31 @@
 
 const init = ["UNICAR1", "UNICAR2", "UNICAR3", "UNICAR4", "UNICAR5"]
-
 const ref = init
-
 const copy = [...init]
 
 const $copyButton = document.querySelector('.copy')
 const $refButton = document.querySelector('.ref')
+
 const $copyWrapper = document.querySelector('.copy-wrapper')
 const $refWrapper = document.querySelector('.ref-wrapper')
+const $initWrapper = document.querySelector('.init-wrapper')
+
+addArray(init, $initWrapper)
 const $init = document.querySelectorAll('.init-wrapper > div')
 
-console.log($init)
+$copyButton.addEventListener('click', (e) => {
+    addArray(copy, $copyWrapper)
+    enableEdit()
+})
 
-$copyButton.addEventListener('click', (e) => addArray(copy, $copyWrapper))
-$refButton.addEventListener('click', (e) => addArray(ref, $refWrapper))
+$refButton.addEventListener('click', (e) => {
+    addArray(ref, $refWrapper)
+    enableEdit()
+})
 
 function addArray(array, wrapper) {
+    wrapper.innerHTML = ''
+
     array.map(x => {
         const $div = document.createElement('div')
         $div.classList.add('init')
@@ -24,8 +33,14 @@ function addArray(array, wrapper) {
         wrapper.appendChild($div)
     })
 
+}
+
+function enableEdit() {
     $init.forEach(init => {
         init.contentEditable = 'true'
     })
-
 }
+
+$init.forEach(init => {
+    init.addEventListener('input', (e) => console.log(e.target.innerText))
+})
