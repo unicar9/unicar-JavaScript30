bf1f4f3c6427a44b4c3b84f22935dd775fd25b3a
Unicar <weijia@baseup.com.au>
2018-02-21 20:34:35 +1100

    Added saveQuote func

--- d15/index.js
+++ d15/index.js
@@ -3,6 +3,9 @@ const $quote = document.querySelector('.quote')
 const $poo = document.querySelector('.poo')
 const $meh = document.querySelector('.meh')
 
+const quotes = JSON.parse(localStorage.getItem('quotes')) || []
+
+
 fetch(endpoint)
     .then(blob => blob.json())
     .then(data => $quote.innerText = data.value)
@@ -13,6 +16,14 @@ function addQuote() {
         .then(data => $quote.innerText = data.value)
 }
 
+function saveQuote() {
+    const currentQuote = $quote.innerText
+    quotes.push(currentQuote)
+    localStorage.setItem('quotes', JSON.stringify(quotes))
+}
 
-$poo.addEventListener('click', addQuote)
+$poo.addEventListener('click', (e) => {
+    saveQuote()
+    addQuote()
+})
 $meh.addEventListener('click', addQuote)
