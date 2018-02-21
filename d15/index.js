3a57a4ac4acee58769720c2c723b9c3c6b7c7717
Unicar <weijia@baseup.com.au>
2018-02-21 21:00:21 +1100

    Adding return button

--- d15/index.js
+++ d15/index.js
@@ -2,6 +2,9 @@ const endpoint = "https://api.tronalddump.io/random/quote"
 const $quote = document.querySelector('.quote')
 const $poo = document.querySelector('.poo')
 const $meh = document.querySelector('.meh')
+const $saved = document.querySelector('.saved')
+const $quoteWrapper = document.querySelector('.quote-wrapper')
+const $savedQuotes = document.querySelector('.saved-quotes')
 
 const quotes = JSON.parse(localStorage.getItem('quotes')) || []
 
@@ -20,10 +23,23 @@ function saveQuote() {
     const currentQuote = $quote.innerText
     quotes.push(currentQuote)
     localStorage.setItem('quotes', JSON.stringify(quotes))
+
+    addQuote()
 }
 
-$poo.addEventListener('click', (e) => {
-    saveQuote()
-    addQuote()
-})
+function showSavedQuotes() {
+    $quoteWrapper.style.display = 'none'
+    quotes.map(quote => {
+        const $div = document.createElement('div')
+        $div.classList.add('quote-wrapper')
+        $div.style.padding = '20px'
+        $div.innerText = quote
+
+        $savedQuotes.appendChild($div)
+    })
+}
+
+
+$poo.addEventListener('click', saveQuote)
 $meh.addEventListener('click', addQuote)
+$saved.addEventListener('click', showSavedQuotes)
