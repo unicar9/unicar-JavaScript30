bc2f6f1566503629ebfb729cf3203cb606ece578
Unicar <weijia@baseup.com.au>
2018-02-21 19:56:53 +1100

    Work on d15

--- d15/index.js
+++ d15/index.js
@@ -1,12 +1,12 @@
 const endpoint = "https://api.tronalddump.io/random/quote"
 const $container = document.querySelector('.container')
 
-const quotes = []
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => {
-        console.log(data)
-    //   quotes.push(...data.data)
-    //   console.log(quotes)
-    //   console.log("Fetch down!!!!")
-    })
+// const quotes = []
+// fetch(endpoint)
+//     .then(blob => blob.json())
+//     .then(data => {
+//         console.log(data)
+//     //   quotes.push(...data.data)
+//     //   console.log(quotes)
+//     //   console.log("Fetch down!!!!")
+//     })
