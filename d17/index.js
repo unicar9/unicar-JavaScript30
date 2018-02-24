38b95876d724d5f79da44e533e119ba29a5bbc11
Unicar <unicar9@gmail.com>
2018-02-25 10:12:14 +1100

    d17 finished

--- d17/index.js
+++ d17/index.js
@@ -1,25 +1,23 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog']
 
+const sortButton = document.querySelector('.button')
+
 function strip(bandName) {
     return bandName.replace(/^(a |the |an )/i, '').trim();
 }
-  
-const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-document.querySelector('#bands').innerHTML =
-  sortedBands
+function sortBands() {
+    const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+    displayList(sortedBands)
+}
+
+function displayList(bandList) {
+  document.querySelector('#bands').innerHTML =
+  bandList
     .map(band => `<li>${band}</li>`)
     .join('');
+}
 
-console.log(sortedBands)
+sortButton.addEventListener('click', sortBands)
 
-const endpoint = 'https://raw.githubusercontent.com/douMax/jsfun30/master/d6/products.json'
-
-const products = []
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => {
-        products.push(...data)
-        console.log(products)
-    })
-
+displayList(bands);
