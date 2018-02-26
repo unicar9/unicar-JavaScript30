cf3485ed1331cea9d2baab9cda80dd9b691cb8d9
Unicar <weijia@baseup.com.au>
2018-02-26 22:01:22 +1100

    added hero png

---
+++ d20/index.js
@@ -0,0 +1,29 @@
+const $maze = document.querySelector('.maze')
+
+const $cell = document.createElement('div')
+
+let $cells = []
+
+
+
+
+
+function reset() {
+    // $maze.innerHTML = ''
+
+    // $cells = Array(25).fill().map((x, index) => {
+    //     const $cell = document.createElement("div")
+
+    //     $cell.style.width = '50px'
+    //     $cell.style.height = '50px'
+    //     $cell.style.lineHeight = '50px'
+    //     $cell.style.fontSize = '50px'
+    //     $cell.style.background = '#333'
+    //     $cell.innerText = 5
+    //     $cell.style.color = `hsl(20, 100%, 80%)`
+
+    //     return $maze.appendChild($cell)
+    // })
+}
+
+reset()
