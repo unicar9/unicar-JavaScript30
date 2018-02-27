412c5ab709093cb6653fde747a17b7a1d598e1d9
Unicar <weijia@baseup.com.au>
2018-02-27 20:59:20 +1100

    Changed direction

--- d20/index.js
+++ d20/index.js
@@ -16,6 +16,43 @@ function verticalMove(dir) {
 function horizontalMove(dir) {
     const colNow = $hero.style.gridColumn.split(' / ')
 
-    const colNext = dir === 'left' ? `${+colNow[0] - 1} / ${+colNow[1] - 1}` : `${+colNow[0] + 1} / ${+colNow[1] + 1}`
+    $hero.style.gridColumn = dir === 'left' ? `${+colNow[0] - 1} / ${+colNow[1] - 1}` : `${+colNow[0] + 1} / ${+colNow[1] + 1}`
 
 }
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+const recognition = new SpeechRecognition()
+recognition.interimResults = false
+recognition.maxAlternatives = 2
+
+// recognition.addEventListener('result', e => {
+//     const transcript = Array.from(e.results)
+//         .map(result => result[0])
+//         .map(result => result.transcript)
+//         .join('')
+    
+    
+// })
+
+recognition.onresult = (e) => {
+    const order = e.results[0][0].transcript.toLowerCase()
+
+    if (order.includes('up')) {
+        verticalMove('up')
+    } 
+    if (order.includes('down')) {
+        verticalMove('down')
+    } 
+    if (order.includes('left')) {
+        horizontalMove('left')
+    } 
+    if (order.includes('right')) {
+        horizontalMove('right')
+    } 
+    console.log(order)
+}
+
+recognition.addEventListener('end', recognition.start)
+
+recognition.start()
