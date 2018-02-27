209ed1beb345516e2cde92d93a3370e11f7d56b3
Unicar <weijia@baseup.com.au>
2018-02-27 22:32:38 +1100

    Added conditions

--- d20/index.js
+++ d20/index.js
@@ -1,5 +1,12 @@
 const $maze = document.querySelector('.maze')
 const $hero = document.querySelector('.hero')
+const $pathDivs = document.querySelectorAll('.path')
+
+const pathGridAreas = Array.from($pathDivs).map(a => window.getComputedStyle(a).gridArea)
+
+console.log(pathGridAreas)
+
+$hero.addEventListener('animationend', (e) => $hero.classList.remove('shake'))
 
 function reset() {
     $hero.style.gridArea = "5/2/6/3"
@@ -7,17 +14,34 @@ function reset() {
 
 reset()
 
+function checkIfOnPath(gridArea) {
+    if (pathGridAreas.includes(gridArea)) return true
+    return false
+}
+
 function verticalMove(dir) {
     const rowNow = $hero.style.gridRow.split(' / ')
-
+    const areaNow = $hero.style.gridArea
+    console.log(areaNow)
     $hero.style.gridRow = dir === 'up' ? `${+rowNow[0] - 1} / ${+rowNow[1] - 1}` : `${+rowNow[0] + 1} / ${+rowNow[1] + 1}` 
+    const areaNext = $hero.style.gridArea
+    if (!checkIfOnPath(areaNext)) {
+        $hero.style.gridArea = areaNow
+        $hero.classList.add('shake')
+    }
 }
 
 function horizontalMove(dir) {
     const colNow = $hero.style.gridColumn.split(' / ')
+    const areaNow = $hero.style.gridArea
+    console.log(areaNow)
 
     $hero.style.gridColumn = dir === 'left' ? `${+colNow[0] - 1} / ${+colNow[1] - 1}` : `${+colNow[0] + 1} / ${+colNow[1] + 1}`
-
+    const areaNext = $hero.style.gridArea
+    if (!checkIfOnPath(areaNext)) {
+        $hero.style.gridArea = areaNow
+        $hero.classList.add('shake')
+    }
 }
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
