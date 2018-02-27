453991ceb242c34c8d76dc10e3f6fb87ea1f3352
Unicar <weijia@baseup.com.au>
2018-02-28 10:23:59 +1100

    Added pin to the maze

--- d20/index.js
+++ d20/index.js
@@ -1,46 +1,51 @@
 const $maze = document.querySelector('.maze')
 const $hero = document.querySelector('.hero')
 const $pathDivs = document.querySelectorAll('.path')
-
 const pathGridAreas = Array.from($pathDivs).map(a => window.getComputedStyle(a).gridArea)
 
-console.log(pathGridAreas)
-
-$hero.addEventListener('animationend', (e) => $hero.classList.remove('shake'))
 
 function reset() {
     $hero.style.gridArea = "5/2/6/3"
 }
-
-reset()
 
 function checkIfOnPath(gridArea) {
     if (pathGridAreas.includes(gridArea)) return true
     return false
 }
 
-function verticalMove(dir) {
-    const rowNow = $hero.style.gridRow.split(' / ')
-    const areaNow = $hero.style.gridArea
-    console.log(areaNow)
-    $hero.style.gridRow = dir === 'up' ? `${+rowNow[0] - 1} / ${+rowNow[1] - 1}` : `${+rowNow[0] + 1} / ${+rowNow[1] + 1}` 
-    const areaNext = $hero.style.gridArea
+
+function move(dir) {
+    const areaNow = $hero.style.gridArea.split(' / ')
+    let areaNext = ''
+    
+    switch (dir) {
+        case 'up':
+        areaNext = areaNow.map((x, i) => i % 2 === 0 ? +x - 1 : x).join(' / ')
+        break;
+        case 'down':
+        areaNext = areaNow.map((x, i) => i % 2 === 0 ? +x + 1 : x).join(' / ')
+        break;
+        case 'left':
+        areaNext = areaNow.map((x, i) => i % 2 !== 0 ? +x - 1 : x).join(' / ')
+        break;
+        case 'right':
+        areaNext = areaNow.map((x, i) => i % 2 !== 0 ? +x + 1 : x).join(' / ')
+        break;
+        default:
+        console.log('aaa')
+    }
+    
+    $hero.style.gridArea = areaNext
+    
     if (!checkIfOnPath(areaNext)) {
-        $hero.style.gridArea = areaNow
+        $hero.style.gridArea = areaNow.join(' / ')
         $hero.classList.add('shake')
     }
 }
 
-function horizontalMove(dir) {
-    const colNow = $hero.style.gridColumn.split(' / ')
-    const areaNow = $hero.style.gridArea
-    console.log(areaNow)
-
-    $hero.style.gridColumn = dir === 'left' ? `${+colNow[0] - 1} / ${+colNow[1] - 1}` : `${+colNow[0] + 1} / ${+colNow[1] + 1}`
-    const areaNext = $hero.style.gridArea
-    if (!checkIfOnPath(areaNext)) {
-        $hero.style.gridArea = areaNow
-        $hero.classList.add('shake')
+function orderToMove(dir, order) {
+    if(order.includes(dir)) {
+        move(dir)
     }
 }
 
@@ -48,35 +53,22 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 
 const recognition = new SpeechRecognition()
 recognition.interimResults = false
-recognition.maxAlternatives = 2
+recognition.maxAlternatives = 1
 
-// recognition.addEventListener('result', e => {
-//     const transcript = Array.from(e.results)
-//         .map(result => result[0])
-//         .map(result => result.transcript)
-//         .join('')
-    
-    
-// })
-
-recognition.onresult = (e) => {
+recognition.onresult = e => {
     const order = e.results[0][0].transcript.toLowerCase()
-
-    if (order.includes('up')) {
-        verticalMove('up')
-    } 
-    if (order.includes('down')) {
-        verticalMove('down')
-    } 
-    if (order.includes('left')) {
-        horizontalMove('left')
-    } 
-    if (order.includes('right')) {
-        horizontalMove('right')
-    } 
+    
+    orderToMove("up", order)
+    orderToMove("down", order)
+    orderToMove("left", order)
+    orderToMove("right", order)
+    
     console.log(order)
 }
 
-recognition.addEventListener('end', recognition.start)
 
+reset()
+$hero.addEventListener('animationend', (e) => $hero.classList.remove('shake'))
+
+recognition.addEventListener('end', recognition.start)
 recognition.start()
