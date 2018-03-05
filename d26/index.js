e3432b27a462a91a17c0fa1a05b3a2f98f891de0
Unicar <unicar9@gmail.com>
2018-03-05 21:55:15 +1100

    Small changes

--- d26/index.js
+++ d26/index.js
@@ -6,8 +6,7 @@ const nav = document.querySelector('.top')
 
 function handleEnter() {
   this.classList.add('trigger-enter')
-  // setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150)
-  this.classList.add('trigger-enter-active')
+  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150)
   background.classList.add('open')
   
   const dropdown = this.querySelector('.dropdown')
