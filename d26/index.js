cb483ce4288d5f79d58a56eecc7f74a1efaf5ab1
Unicar <unicar9@gmail.com>
2018-03-05 21:54:06 +1100

    Added

---
+++ d26/index.js
@@ -0,0 +1,38 @@
+console.log('haha')
+
+const menuItems = document.querySelectorAll('.cool > li')
+const background = document.querySelector('.dropdownBackground')
+const nav = document.querySelector('.top')
+
+function handleEnter() {
+  this.classList.add('trigger-enter')
+  // setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150)
+  this.classList.add('trigger-enter-active')
+  background.classList.add('open')
+  
+  const dropdown = this.querySelector('.dropdown')
+  const dropdownCoords = dropdown.getBoundingClientRect()
+  const navCoords = nav.getBoundingClientRect()
+  
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left
+  }
+  
+  background.style.width = `${coords.width}px`
+  background.style.height = `${coords.height}px`
+  background.style.transform = `translate(${coords.left}px, ${coords.top}px)`
+  
+}
+
+function handleLeave() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active')
+  background.classList.remove('open')
+}
+
+menuItems.forEach(item => {
+  item.addEventListener('mouseenter', handleEnter)
+  item.addEventListener('mouseleave', handleLeave)
+})
