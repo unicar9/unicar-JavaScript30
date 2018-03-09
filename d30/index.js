7d783be57a51f77d0ac92f97ef39c9835bae7343
Unicar <unicar9@gmail.com>
2018-03-09 20:27:45 +1100

    Added cursor animation

--- d30/index.js
+++ d30/index.js
@@ -1,20 +1,38 @@
 
-var FizzyText = function() {
-    this.message = 'dat.gui';
-    this.speed = 0.8;
-    this.displayOutline = false;
-    // this.explode = function() {  };
-    // Define render logic ...
-}
+// var FizzyText = function() {
+//     this.message = 'dat.gui';
+//     this.speed = 0.8;
+//     this.displayOutline = false;
+//     // this.explode = function() {  };
+//     // Define render logic ...
+// }
 
 
 
-window.onload = function() {
-    var text = new FizzyText();
-    var gui = new dat.GUI();
-    gui.add(text, 'message');
-    gui.add(text, 'speed', -5, 5);
-    gui.add(text, 'displayOutline');
-    gui.add(text, 'explode');
-}
+// window.onload = function() {
+//     var text = new FizzyText();
+//     var gui = new dat.GUI();
+//     gui.add(text, 'message');
+//     gui.add(text, 'speed', -5, 5);
+//     gui.add(text, 'displayOutline');
+//     gui.add(text, 'explode');
+// }
 
+const malletCursor = document.querySelector('.mallet')
+
+window.addEventListener('mousemove', (e) => {
+    console.log(e.pageX, e.pageY)
+    malletCursor.style.left = `${e.pageX}px`
+    malletCursor.style.top = `${e.pageY}px`
+})
+
+
+window.addEventListener('mousedown', ()=> {
+    malletCursor.classList.add('hit')
+})
+
+malletCursor.addEventListener('transitionend', () => malletCursor.classList.remove('hit'))
+
+// window.addEventListener('mouseup', ()=> {
+//     malletCursor.classList.remove('hit')
+// })
